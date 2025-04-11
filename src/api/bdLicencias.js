54f1ea4b375bb8256874c67fe63d03913ce8dda1
Alvaro Japa Salazar <alvarojapasalazar@MacBook-Air-de-Alvaro.local>
2025-04-11 14:59:28 -0500

    apuntando a servidor

--- src/api/bdLicencias.js
+++ src/api/bdLicencias.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const bdLicencias = axios.create({
     // baseURL: 'https://sv-yptplguxwm.cloud.elastika.pe/api'
-     baseURL: 'http://127.0.0.1:8000/api'
+     baseURL: 'http://sv-nlaqt2o6tr.cloud.elastika.pe:81/api'
 })
 
 export default bdLicencias
